b65be54bab5ab727b903ba59f1e2102134f23799
Cat-Mills <141058467+Cat-Mills@users.noreply.github.com>
2023-08-04 12:31:14 -0600

    finished with js2

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -9,7 +9,7 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+const gitDefinition = "Git is the home or 'storage cabinet' for all your code folders or repositories."
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,7 +17,7 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+const gitHubDefinition = "GitHub is the access point for all your saved and stored repositories. It saves your data in a cloud accessible from any computer."
 
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
@@ -33,7 +33,10 @@
     following the guidelines above to describe the init command.
 */
 
-//CODE HERE
+const init = {
+    description: "Creates local repository in selected folder",
+    code: "git init"
+}
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -41,7 +44,13 @@
     following the guidelines above to describe the clone command.
 */
 
-//CODE HERE
+const clone = {
+    description: "Creates a copy of a specific repository or branch inside a repository",
+    code: "git clone [url]" // Other commands include:
+    // "git clone --mirror" = Clones a repo but without the ability to edit any of the files.
+    // "git clone --single-branch" = Clones a single branch within repo.
+    // "git clone --sparse" = Only populates the files in the root directory. Good when cloning large repos with many directories and sub-directories.
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -49,7 +58,10 @@
     following the guidelines above to describe the status command.
 */
 
-//CODE HERE
+const status = {
+    description: "displays the status of the working directory. It lets you see which changes have been staged, which haven't, and which files aren't being tracked by Git.",
+    code: "git status"
+}
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -57,7 +69,10 @@
     following the guidelines above to describe the add command.
 */
 
-//CODE HERE
+const add = {
+    description: "tells Git that you want to include updates to a certain file in the next commit",
+    code: "git add" //"git add ." will add any new updates to commit
+}
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -65,7 +80,10 @@
     following the guidelines above to describe the commit command.
 */
 
-//CODE HERE
+const commit = {
+    description: "this is like a snapshot of your repo. It takes all the data in your repo and saves it at a specific time",
+    code: 'git commit -m "(message goes here)"'
+}
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,7 +91,10 @@
     following the guidelines above to describe the command to add a remote location to your git repository.
 */
 
-//CODE HERE
+const addRemote = {
+    description: "adds a remote URL to your repo that allows you to link and access it in GitHub or on a different server.", //remote URL is Git's way of saying "the place where your code is stored"
+    code: "git remote add origin" //Git associates a remote URL with a name, and your default remote is usually called origin. You can use the command "git remote set-url" to change a remotes URL.
+}
 
 //////////////////PROBLEM 9////////////////////
 /*
@@ -81,4 +102,7 @@
     following the guidelines above to describe the push command.
 */
 
-//CODE HERE
+const push = {
+    description: "used to upload the content from your local to your remote repo by transferring commits",
+    code: "git push"
+}
